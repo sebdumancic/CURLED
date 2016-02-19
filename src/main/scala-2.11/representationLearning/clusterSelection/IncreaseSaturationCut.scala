daac11156735105d4c7acc5bcf2117e84454ef04
Sebac <sebastijan.dumancic@gmail.com>
2016-02-19 13:45:38 +0100

    added saturation cut
    Former-commit-id: 90eb2f03c5996e668eaeb4d4d6e6cdd807293ac1

---
+++ src/main/scala-2.11/representationLearning/clusterSelection/IncreaseSaturationCut.scala
@@ -0,0 +1,29 @@
+package representationLearning.clusterSelection
+
+import relationalClustering.clustering.evaluation.AbstractEvaluatorModel
+
+/**
+  * Created by seb on 19.02.16.
+  */
+class IncreaseSaturationCut(override protected val similarityMatrixFileName: String,
+                            override protected val elementOrdering: List[String],
+                            override protected val evaluateSingle: AbstractEvaluatorModel,
+                            protected val factor: Double) extends ModelBasedSelection(similarityMatrixFileName, elementOrdering, evaluateSingle) {
+
+  /** Selects the first cluster for which the following equation holds:
+    * sim(C(i)) >= factor * sim(C(i+1))
+    *
+    * assumes that clusters are listed in the increasing order and that the score increases with the number of clusters.
+    * If none of the clusters satisfies the equation, returns the clustering with the highest score
+    * */
+  override def selectFromClusters(clusterSet: List[Set[List[String]]]) = {
+    val evals = clusterSet.map( cluster => evaluateSingle.validate(cluster, getElementOrder, getSimilarityMatrixFile)).zipWithIndex
+    val cands = evals.dropRight(1).map( item => item._1 >= (factor * evals(item._2 + 1)._1)).indexOf(true)
+
+    cands == -1 match {
+      case false => clusterSet(cands)
+      case true => clusterSet.maxBy(cluster => evaluateSingle.validate(cluster, getElementOrder, getSimilarityMatrixFile))
+    }
+  }
+
+}
