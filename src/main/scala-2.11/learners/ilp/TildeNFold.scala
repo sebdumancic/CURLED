7eedac265a003ccb33eaa7a3a02d85f4bc7cc077
Sebac <sebastijan.dumancic@gmail.com>
2016-02-24 16:02:18 +0100

    TILDE wrappers added
    Former-commit-id: 8566d41d7cc44f4e2c1a028648d193852ea149a5

---
+++ src/main/scala-2.11/learners/ilp/TildeNFold.scala
@@ -0,0 +1,31 @@
+package learners.ilp
+
+import relationalClustering.representation.KnowledgeBase
+
+/**
+  * Created by seb on 24.02.16.
+  */
+class TildeNFold(override protected val rootFolder: String,
+                 override protected val knowledgeBase: KnowledgeBase,
+                 override protected val latentKnowledgeBase: KnowledgeBase,
+                 override protected val targetPredicate: String,
+                 override protected val isRelation: Boolean,
+                 override protected val aceRootFolder: String,
+                 protected val numFolds: Int,
+                 override protected val heuristic: String = "gain",
+                 override protected val minCases: Int = 4) extends TildeInduce(rootFolder, knowledgeBase, latentKnowledgeBase, targetPredicate, isRelation, aceRootFolder, heuristic, minCases){
+
+  /** Returns the number of folds*/
+  protected def getNumberOfFolds = {
+    numFolds
+  }
+
+  override def runCommand = {
+    s"nfold(tilde,$getNumberOfFolds)"
+  }
+
+  override def getFilesWithRules = {
+    (1 to getNumberOfFolds).toList.map( fold => s"tilde/${getTargetPredicateName.toLowerCase}.uB$fold")
+  }
+
+}
