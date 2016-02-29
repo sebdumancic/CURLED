d2affd8bf752300e755420964c5c99462c557849
Sebac <sebastijan.dumancic@gmail.com>
2016-02-29 15:47:49 +0100

    abstract layer added
    Former-commit-id: 8896a9fc1e8d47f814c8190a08d6999b6d0760f4

---
+++ src/main/scala-2.11/representationLearning/layer/AbstractLayer.scala
@@ -0,0 +1,45 @@
+package representationLearning.layer
+
+import java.io.{BufferedWriter, FileWriter}
+
+/**
+  * Created by seb on 29.02.16.
+  */
+abstract class AbstractLayer(protected val rootFolder: String,
+                             protected val outputName: String) {
+
+  protected val headerFile = new BufferedWriter(new FileWriter(s"$getRoot/$getOutputName.def"))
+  protected val declarationsFile = new BufferedWriter(new FileWriter(s"$getRoot/$getOutputName.decl"))
+  protected val kbFile = new BufferedWriter(new FileWriter(s"$getRoot/$getOutputName.kb"))
+
+  /** Returns the root folder */
+  protected def getRoot = {
+    rootFolder
+  }
+
+  /** Returns the output first */
+  protected def getOutputName = {
+    outputName
+  }
+
+  /** Returns the header file [[BufferedWriter]] */
+  protected def getHeaderFile = {
+    headerFile
+  }
+
+  /** Returns the declarations file [[BufferedWriter]] */
+  protected def getDeclFile = {
+    declarationsFile
+  }
+
+  /** Returns the knowledge base file [[BufferedWriter]] */
+  protected def getKBFile = {
+    kbFile
+  }
+
+  /** Method to build the layer
+    *
+    * @return (header file name, kb file name, declarations filename)
+    **/
+  def build(): (String, String, String)
+}
