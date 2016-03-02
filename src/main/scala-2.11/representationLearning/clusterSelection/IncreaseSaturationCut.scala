e322c42541e14442491d097a500d8103eaafc412
Sebac <sebastijan.dumancic@gmail.com>
2016-03-02 14:10:33 +0100

    commented out
    Former-commit-id: 494975042088963a07f5025ee886b00a5f195c45

--- src/main/scala-2.11/representationLearning/clusterSelection/IncreaseSaturationCut.scala
+++ src/main/scala-2.11/representationLearning/clusterSelection/IncreaseSaturationCut.scala
@@ -2,17 +2,22 @@ package representationLearning.clusterSelection
 
 import relationalClustering.clustering.evaluation.AbstractEvaluatorModel
 
-/**
+/** Selects the first cluster for which the following equation holds:
+  * sim(C(i)) >= factor * sim(C(i+1))
+  *
+  * assumes that clusters are listed in the increasing order and that the score increases with the number of clusters.
+  * If none of the clusters satisfies the equation, returns the clustering with the highest score
+  * *
   * Created by seb on 19.02.16.
   */
 class IncreaseSaturationCut(protected val evaluateSingle: AbstractEvaluatorModel,
                             protected val factor: Double) extends AbstractClusterSelection {
 
-  /** Selects the first cluster for which the following equation holds:
-    * sim(C(i)) >= factor * sim(C(i+1))
+  /** Applies the evaluation procedure
     *
-    * assumes that clusters are listed in the increasing order and that the score increases with the number of clusters.
-    * If none of the clusters satisfies the equation, returns the clustering with the highest score
+    * @param clusterSet               a set of clusterings
+    * @param elementOrdering          an ordering of the elements in the similarity matrix
+    * @param similarityMatrixFileName the name of the file containing the similarity matrix
     * */
   override def selectFromClusters(clusterSet: List[Set[List[String]]], elementOrdering: List[String], similarityMatrixFileName: String) = {
 
