aef5e180f16fa0b9fd84f7db4f789454157b8989
Sebac <sebastijan.dumancic@gmail.com>
2016-03-02 14:19:50 +0100

    max layer construction method added - selects the clustering that achieves the highest score
    Former-commit-id: b1173273b4604691876f3177191363e5d76f3f2e

---
+++ src/main/scala-2.11/representationLearning/layer/MaxLayer.scala
@@ -0,0 +1,32 @@
+package representationLearning.layer
+
+import relationalClustering.bagComparison.AbstractBagComparison
+import relationalClustering.bagComparison.bagCombination.AbstractBagCombine
+import relationalClustering.clustering.AbstractSKLearnCluster
+import relationalClustering.clustering.evaluation.AbstractEvaluatorModel
+import relationalClustering.representation.KnowledgeBase
+import representationLearning.clusterComparison.AbstractClusterOverlap
+import representationLearning.clusterSelection.ModelBasedSelection
+
+/**
+  * Created by seb on 02.03.16.
+  */
+class MaxLayer(override protected val rootFolder: String,
+               override protected val outputName: String,
+               override protected val knowledgeBase: KnowledgeBase,
+               override protected val domainsToCluster: List[String],
+               override protected val depth: Int,
+               override protected val bagCompare: AbstractBagComparison,
+               override protected val bagCombination: AbstractBagCombine,
+               override protected val measureIdentifier: String,
+               override protected val clusteringAlg: AbstractSKLearnCluster,
+               protected val clusterEval: AbstractEvaluatorModel,
+               override protected val clusterOverlap: AbstractClusterOverlap,
+               override protected val overlapThreshold: Double,
+               override protected val maxClusters: Int,
+               override protected val parameterList: List[List[Double]],
+               override protected val clusterHyperedges: Boolean) extends AdaptiveSelectionLayer(rootFolder, outputName, knowledgeBase, domainsToCluster, depth, bagCompare, bagCombination,
+  measureIdentifier, clusteringAlg, new ModelBasedSelection(clusterEval), clusterOverlap,
+  overlapThreshold, maxClusters, parameterList, clusterHyperedges) {
+
+}
