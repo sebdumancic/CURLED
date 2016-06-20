768aeba38b1d9bafd96a4785c26a222b97825610
sdumancic <sebastijan.dumancic@gmail.com>
2016-06-20 17:08:07 +0200

    Cluster selection  methods work on Clustering

--- src/main/scala-2.11/representationLearning/clusterSelection/AbstractClusterSelection.scala
+++ src/main/scala-2.11/representationLearning/clusterSelection/AbstractClusterSelection.scala
@@ -1,5 +1,7 @@
 package representationLearning.clusterSelection
 
+import relationalClustering.representation.clustering.Clustering
+
 /**
   * Created by seb on 09.02.16.
   */
@@ -8,8 +10,6 @@ abstract class AbstractClusterSelection {
   /** Selects the best clustering according to a certain score
     *
     * @param clusterSet               an ordered set of clusterings
-    * @param elementOrdering          an ordering of elements in a similarity matrix
-    * @param similarityMatrixFileName a file containing the similarity matrix
     **/
-  def selectFromClusters(clusterSet: List[Set[List[String]]], elementOrdering: List[String], similarityMatrixFileName: String): Set[List[String]]
+  def selectFromClusters(clusterSet: List[Clustering]): Clustering
 }
