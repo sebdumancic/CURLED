a74a59b7d16c9065ccc404f0e5a457dc025c84d3
sdumancic <sebastijan.dumancic@gmail.com>
2017-03-31 17:37:22 +0200

    representation stats finished (naive version)

--- src/main/scala-2.11/reports/RepresentationStats.scala
+++ src/main/scala-2.11/reports/RepresentationStats.scala
@@ -3,8 +3,9 @@ package reports
 import java.io.{BufferedWriter, FileWriter}
 
 import relationalClustering.clustering.evaluation.LabelsContainer
-import relationalClustering.representation.clustering.Cluster
+import relationalClustering.representation.clustering.{Cluster, Clustering}
 import relationalClustering.representation.domain.{KnowledgeBase, Predicate}
+import representationLearning.clusterComparison.OverlapWithARI
 import representationLearning.representation.ClusteringRepresentation
 import vegas._
 
@@ -23,11 +24,12 @@ class RepresentationStats(protected val kb: KnowledgeBase,
       render(getPurity, "predicate", "purity", "representation", "class purity", s"$folder/classPurity.html")
     }
 
-    render(getOriginalPredicateInteraction, "predicate1", "predicate2", "interaction", "original predicate interaction", s"$folder/originalInteraction.html")
-    render(getLatentPredicateInteraction, "predicate1", "predicate2", "interaction", "latent predicate interaction", s"folder/latentInteraction.html")
+    render(getPredicateInteraction, "predicate1", "predicate2", "interaction", "representation", "original predicate interaction", s"$folder/predicateInteraction.html")
+    render(getLatentRedundancy(folder), "ARI", "reduction", "aspect", "redundancy reduction", s"$folder/latentRedundancy.html")
   }
 
   protected def render(data: Seq[Map[String,Any]], axisX: String, axisY: String, color: String, name: String, filename: String): Unit = {
+    // TODO: add automatic detection of data type for Vegas
     val plot = Vegas(name).
       withData(data).
       encodeX(axisX, Ord).
@@ -44,11 +46,27 @@ class RepresentationStats(protected val kb: KnowledgeBase,
     jsonFile.close()
   }
 
+  protected def render(data: Seq[Map[String,Any]], axisX: String, axisY: String, color: String, row: String, name: String, filename: String): Unit = {
+    val plot = Vegas(name).
+      withData(data).
+      encodeX(axisX, Ord).
+      encodeY(axisY, Quant).
+      encodeColor(color, Nom).
+      encodeRow(row, Nom).
+      mark(Bar)
+
+    val fileWriter = new BufferedWriter(new FileWriter(filename))
+    fileWriter.write(plot.html.pageHTML())
+    fileWriter.close()
+
+    val jsonFile = new BufferedWriter(new FileWriter(s"$filename.json"))
+    jsonFile.write(plot.toJson)
+    jsonFile.close()
+  }
+
   protected def getNumberOfTrueGroundings: Seq[Map[String,Any]] = {
 
-    val originalPreds = kb.getPredicateNames.map(kb.getPredicate).map(p => (p.getName, p.getTrueGroundings.size, p.getDomains)).map(item => {
-      Map("predicate" -> item._1, "count" -> item._2, "representation" -> "original", "domains" -> item._3)
-    })
+    val originalPreds = kb.getPredicateNames.map(kb.getPredicate).map(p =>  Map("predicate" -> p.getName, "count" -> p.getTrueGroundings.size, "representation" -> "original", "domains" -> p.getDomains))
     val latentPreds = latentRepresentation.getClusterings.foldLeft(Seq[Map[String, Any]]())((acc, cl) => {
       acc ++ cl.getClusters.map( p => {
         Map("predicate" -> p.getClusterName, "count" -> p.getSize, "representation" -> "latent", "domains" -> p.getTypes)
@@ -95,17 +113,18 @@ class RepresentationStats(protected val kb: KnowledgeBase,
   }
 
   protected def getOriginalPredicateInteraction: Seq[Map[String,Any]] = {
-    kb.getPredicateNames.map(kb.getPredicate).combinations(2).foldLeft(Map[(String,String), Double]())((acc, pComb) => {
+    kb.getPredicateNames.map(kb.getPredicate).combinations(2).foldLeft(Map[(Predicate,Predicate), Double]())((acc, pComb) => {
       val p1 = pComb.head
       val p2 = pComb(1)
-      val denominator = List(p1.getTrueGroundings.size,p2.getTrueGroundings.size).max
+      var denominator = 0
 
       if (p1.getDomains.intersect(p2.getDomains).isEmpty) {
-        acc + ((p1.getName, p2.getName) -> 0.0)
+        acc + ((p1, p2) -> 0.0)
       }
       else{
-        acc + ( (p1.getName, p2.getName) -> p1.getTrueGroundings.foldLeft(0.0)((acc_1, p1gr) => {
+        acc + ( (p1, p2) -> p1.getTrueGroundings.foldLeft(0.0)((acc_1, p1gr) => {
           acc_1 + p2.getTrueGroundings.foldLeft(0.0)((acc_2, p2gr) => {
+            denominator = denominator + 1
             if (p1gr.exists(el => p2gr.contains(el))) {
               acc_2 + 1.0
             }
@@ -115,21 +134,23 @@ class RepresentationStats(protected val kb: KnowledgeBase,
           })
         })/denominator)
       }
-    }).map(item => Map("predicate1" -> item._1._1, "predicate2" -> item._1._2, "interaction" -> item._2, "representation" -> "original")).toList
+    }).flatMap(item => Seq(Map("predicate1" -> item._1._1, "predicate2" -> item._1._2, "interaction" -> item._2, "representation" -> "original", "count1" -> item._1._1.getTrueGroundings.size, "count2" -> item._1._2.getTrueGroundings.size),
+                           Map("predicate2" -> item._1._1, "predicate1" -> item._1._2, "interaction" -> item._2, "representation" -> "original", "count2" -> item._1._1.getTrueGroundings.size, "count1" -> item._1._2.getTrueGroundings.size))).toList
   }
 
   protected def getLatentPredicateInteraction: Seq[Map[String,Any]] = {
-    latentRepresentation.getClusterings.foldLeft(List[Cluster]())((acc, clustrep) => acc ++ clustrep.getClusters).combinations(2).foldLeft(Map[(String,String), Double]())((acc, clComb) => {
+    latentRepresentation.getClusterings.foldLeft(List[Cluster]())((acc, clustrep) => acc ++ clustrep.getClusters).combinations(2).foldLeft(Map[(Cluster,Cluster), Double]())((acc, clComb) => {
       val cl1 = clComb.head
       val cl2 = clComb(1)
-      val denominator = List(cl1.getSize, cl2.getSize).max
+      var denominator = 0
 
       if (cl1.getTypes.intersect(cl2.getTypes).isEmpty) {
-        acc + ((cl1.getClusterName, cl2.getClusterName) -> 0.0)
+        acc + ((cl1, cl2) -> 0.0)
       }
       else {
-        acc + ((cl1.getClusterName, cl2.getClusterName) -> cl1.getInstances.foldLeft(0.0)((acc_1, cl1gr) => {
+        acc + ((cl1, cl2) -> cl1.getInstances.foldLeft(0.0)((acc_1, cl1gr) => {
           acc_1 + cl2.getInstances.foldLeft(0.0)((acc_2, cl2gr) => {
+            denominator = denominator + 1
             if (cl1gr.exists(el => cl2gr.contains(el))) {
               acc_2 + 1.0
             }
@@ -139,7 +160,43 @@ class RepresentationStats(protected val kb: KnowledgeBase,
           })
         })/denominator)
       }
-    }).toList.map(item => Map("predicate1" -> item._1._1, "predicate2" -> item._1._2, "interaction" -> item._2, "representation" -> "latent"))
+    }).toList.flatMap(item => Seq(Map("predicate1" -> item._1._1.getClusterName, "predicate2" -> item._1._2.getClusterName, "interaction" -> item._2, "representation" -> "latent", "count1" -> item._1._1.getSize, "count2" -> item._1._2.getSize),
+                                  Map("predicate2" -> item._1._1.getClusterName, "predicate1" -> item._1._2.getClusterName, "interaction" -> item._2, "representation" -> "latent", "count2" -> item._1._1.getSize, "count1" -> item._1._2.getSize)))
+  }
+
+  protected def getLatentRedundancy(folder: String): Seq[Map[String,Any]] = {
+    val distinctTypes = latentRepresentation.getClusterings.map(_.getTypes)
+    val featureDenominator = latentRepresentation.getClusterings.map(_.getClusters.length).sum
+    val factDenominator = latentRepresentation.getClusterings.map(_.getClusters.map(_.getSize).sum).sum
+    val comparator = new OverlapWithARI(folder)
+
+    List(0.9, 0.8, 0.7, 0.6, 0.5).foldLeft(Seq[Map[String,Any]]())((acc, ari) => {
+      var clusterings = List[Clustering]()
+
+      distinctTypes.foreach(dt => {
+        val currentClusterings = latentRepresentation.getClusterings.filter(_.getTypes == dt).toList
+        val overlaps = collection.mutable.Map[Clustering, List[Clustering]]()
+
+        (0 until currentClusterings.size - 1).foreach(cl1ind => {
+          val overlapping = (cl1ind until currentClusterings.size).foldLeft(List[Clustering]())((acc, cl2ind) => {
+            if (comparator.compare(currentClusterings(cl1ind), currentClusterings(cl2ind)) > ari) {
+              acc :+ currentClusterings(cl2ind)
+            }
+            else {
+              acc
+            }
+          })
+          if (overlapping.nonEmpty) {
+            overlaps(currentClusterings(cl1ind)) = overlapping
+          }
+        })
+        val rejected = overlaps.values.flatten.toSet
+        clusterings = clusterings ++ currentClusterings.filterNot(cl => rejected.contains(cl))
+      })
+
+      acc ++ Seq(Map("ARI" -> ari, "reduction" -> clusterings.map(_.getClusters.length).sum.toDouble/featureDenominator, "aspect" -> "feature"),
+                 Map("ARI" -> ari, "reduction" -> clusterings.map(_.getClusters.map(_.getSize).sum).sum.toDouble/factDenominator, "aspect" -> "fact"))
+    })
   }
 
 }
